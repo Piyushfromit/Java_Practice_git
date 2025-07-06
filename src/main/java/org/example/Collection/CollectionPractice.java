ea1821b0f793d31838bf923bd665e6b130352e92
piyushfromit <piyushfrommasai@gmail.com>
2025-07-07 02:14:36 +0530

    jhfh

---
+++ src/main/java/org/example/Collection/CollectionPractice.java
@@ -0,0 +1,15 @@
+package org.example.Collection;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CollectionPractice implements Collection, Iterable {
+
+    Map
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
