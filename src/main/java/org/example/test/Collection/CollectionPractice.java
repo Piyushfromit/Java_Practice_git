8dc1165129d2c950604c6481c997ab24c95ba58c
Piyush <piyush.kumar@sbsind.in>
2026-07-17 10:51:48 +0530

    off

--- src/main/java/org/example/test/Collection/CollectionPractice.java
+++ src/main/java/org/example/test/Collection/CollectionPractice.java
@@ -3,6 +3,7 @@ package org.example.test.Collection;
 
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 public class CollectionPractice  {
 
@@ -16,4 +17,27 @@ public class CollectionPractice  {
     List<String> name2 = new Stack<>();
 
     Set<String> names3 = new CopyOnWriteArraySet<>();
+
+
+    public static void main(String[] args) {
+        String str = "apple";
+
+        String result = str.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .distinct()
+                .collect(Collectors.joining());
+
+        System.out.println(result);
+
+        List<Integer> list = Arrays.asList(4, 6, 3, 7, 89, 3, 5, 455, 23, 28);
+        int sum = list.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println(sum);
+
+
+
+
+    }
 }
