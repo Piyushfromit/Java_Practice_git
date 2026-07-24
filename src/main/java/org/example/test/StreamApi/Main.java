f6591e6dea66f66a888bd2b18833341e27f54d0e
Piyush <piyush.kumar@sbsind.in>
2026-07-24 18:58:52 +0530

    jhfasdha

--- src/main/java/org/example/test/StreamApi/Main.java
+++ src/main/java/org/example/test/StreamApi/Main.java
@@ -122,5 +122,8 @@ public class Main {
 
         System.out.println(result);
 
+
+
+
     }
 }
---
+++ src/main/java/org/example/test/StreamApi5/PracticeClass.java
@@ -0,0 +1,20 @@
+package org.example.test.StreamApi5;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PracticeClass {
+
+    public static void main(String[] args) {
+
+        List<Integer> num = Arrays.asList(3,78 ,57,54,6,456,45,45,454,55,675,67,8,79);
+
+       // Integer sum =
+
+
+    }
+
+
+
+}
